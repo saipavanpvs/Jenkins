14837ccc645fe1a4fc0bb062dd197cf93e9e03dc
Venkata Sai Pavan Kumar Pothuri <vepo0622@netcracker.com>
2024-10-18 16:28:43 +0530

    message

---
+++ LaunchInstance_ec2.sh
@@ -0,0 +1,12 @@
+pipeline {
+    agent any
+    stages {
+        stage('Launch EC2 Instance') {
+            steps {
+                script {
+                    sh './LaunchInstance_ec2.sh'
+                }
+            }
+        }
+    }
+}
